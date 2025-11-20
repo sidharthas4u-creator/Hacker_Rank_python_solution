0609ebe8259ff67f0bc87f4ce616c1c39969c612
sidharth <sidharthas4u@gmail.com>
2025-11-20 14:48:46 +0530

    Create A_Very_Big_Sum.py
    hacker rank solution

---
+++ python problem/A_Very_Big_Sum.py
@@ -0,0 +1,18 @@
+def aVeryBigSum(ar):
+    # Write your code here
+    s=0
+    for i in ar:
+        s+=i
+    return s
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
